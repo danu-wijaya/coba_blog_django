1d1f1dea5e1719acfe8ff63877e6ed9601e85949
Kamandanu Tresna Wijaya <kamandanu.wijaya@gmail.com>
2023-06-14 23:28:55 +0700

    init blog

---
+++ theblog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    body = models.TextField(null=True)
+
+    def __str__(self):
+        return f"Title : {self.title} - Author : {self.author}"
---
+++ theblog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import HomeView
+
+urlpatterns = [
+    # path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
+]
